6a56c085866ec25256058900e038d34b3cc3af31
Mr Lu <lujunfeng00@gmail.com>
2017-08-21 21:42:30 +0800

    配置logback日志

--- README.md
+++ README.md
@@ -4,24 +4,14 @@
 
 1. authorization:权限管理
 
-   1. api:登录接口、角色、用户管理等
+   1. api:登录接口
    1. shiro:shiro权限配置
 
 1. core:工具包、通用基类
 
-1. persistence:持久层（目前只用mybatis）
-
-   1. datasource:数据源配置
-   1. mybatis:mybatis框架配置
-
 1. starter:启动配置
 
 1. system:系统模块
-
-1. web:web、mvc配置
-
-
-
 
 #### .gitignore 文件不起作用
 
---
+++ mlgb-starter/src/main/java/cn/mlgbteam/starter/web/WebMvcConfigurer.java
@@ -0,0 +1,21 @@
+package cn.mlgbteam.starter.web;
+
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import java.util.List;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+        //自定义配置...
+        //FastJsonConfig config = new FastJsonConfig();
+        //config.set ...
+        //converter.setFastJsonConfig(config);
+        converters.add(converter);
+    }
+}
