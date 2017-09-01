cb149334da53f41e831bced36537261a9bc1f04b
Mr Lu <lujunfeng00@gmail.com>
2017-09-01 19:13:11 +0800

    框架测试用例

--- mlgb-starter/src/test/java/cn/mlgbteam/starter/MybatisTest.java
+++ mlgb-starter/src/test/java/cn/mlgbteam/starter/MybatisTest.java
@@ -3,6 +3,9 @@ package cn.mlgbteam.starter;
 import cn.mlgbteam.authorization.api.entity.User;
 import cn.mlgbteam.authorization.api.service.UserService;
 import com.github.pagehelper.Page;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,25 +21,46 @@ public class MybatisTest {
 
     @Test
     public void testInsert() {
-        User user = new User();
+        final User user = new User();
         //user.setId(UUID.randomUUID().toString());
         user.setName("李四");
         user.setAge(23);
-        userService.insertUser(user);
+        this.userService.insertUser(user);
         System.out.println(user.toString());
     }
 
     @Test
     public void testSelect() {
-        Page<User> userPage = userService.listUser();
+        final Page<User> userPage = this.userService.listUser();
 
-        for (User user : userPage) {
+        for (final User user : userPage) {
             System.out.println(user.toString());
         }
 
         System.out.println(userPage.getTotal());
         System.out.println(userPage.toString());
 
+    }
+
+    @Test
+    public void testInsertList() {
+        final List<User> userList = new ArrayList<>();
+        final User user1 = new User();
+        user1.setId(UUID.randomUUID().toString());
+        user1.setName("李四");
+        user1.setAge(23);
+        final User user2 = new User();
+        user2.setId(UUID.randomUUID().toString());
+        user2.setName("王五");
+        user2.setAge(24);
+        userList.add(user1);
+        userList.add(user2);
+
+        this.userService.insertUserList(userList);
+
+        for (final User user : userList) {
+            System.out.println(user.toString());
+        }
     }
 
 }
